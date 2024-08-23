d0937d2e0fe98c8cfaad0d5f9e543bc1b94c0584
slavavorobev <--auto-->
2024-08-23 22:48:37 +0800

    2024-08-23-updates

--- install.sh
+++ install.sh
@@ -1,10 +1,12 @@
 #!/bin/sh
 
-mkdir $HOME/.config
-mkdir $HOME/.local/bin
-
+TERM_UTILS="server-dots"
 CFG=$HOME/.config
+LOCAL_BIN=$HOME/.local/bin
+
+mkdir $LOCAL_BIN 2> /dev/null
 mkdir $CFG 2> /dev/null
+
 CUR=$(pwd)
 
 stow --adopt -vt $CFG .config
@@ -14,12 +16,14 @@ ln -snf $CUR/.tmux.conf $HOME/
 git config user.name "$USER"
 git config user.email "--auto--"
 
-git submodule add https://github.com/owpk/server-dots
+git submodule add https://github.com/owpk/$TERM_UTILS
 git submodule init
 
 stow --adopt -vt $LOCAL_BIN scripts 
 
-./server-dots/install.sh
-
 # Создаем и переключаемся на новую ветку в основном проекте
 git checkout -b "$USER"
+
+cd $TERM_UTILS
+./install.sh
+
