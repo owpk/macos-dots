967e8a622520bc904cb239da5ffc376f49957c9f
owpk <53643556+owpk@users.noreply.github.com>
2024-08-23 13:58:35 +0800

    2024-08-23-updates

--- install.sh
+++ install.sh
@@ -11,5 +11,19 @@ ln -s $CUR/.zshenv $HOME/
 ln -s $CUR/.tmux.conf $HOME/
 
 git submodule add https://github.com/owpk/server-dots
+git submodule init
 
 cp scripts/* $HOME/.local/bin
+
+./server-dots/install-wo-githooks.sh
+
+# Название новой ветки
+NEW_BRANCH="$(cat /proc/sys/kernel/hostname | awk '{print tolower($0)}')"
+
+# Создаем и переключаемся на новую ветку в основном проекте
+git checkout -b "$NEW_BRANCH"
+
+# Проходим по каждому сабмодулю и создаем ветку с тем же именем
+git submodule foreach --recursive '
+    git checkout -b "$NEW_BRANCH"
+'
