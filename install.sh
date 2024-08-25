c2434b95f7525e601713c706d1d726c55753a09c
slavavorobev <--auto-->
2024-08-25 23:14:33 +0800

    update install script

--- install.sh
+++ install.sh
@@ -7,19 +7,14 @@ LOCAL_BIN=$HOME/.local/bin
 mkdir $LOCAL_BIN 2> /dev/null
 mkdir $CFG 2> /dev/null
 
-CUR=$(pwd)
-
 stow --adopt -vt $CFG .config
-ln -snf $CUR/.zshenv $HOME/
-ln -snf $CUR/.tmux.conf $HOME/
+stow --adopt -vt $LOCAL_BIN scripts 
 
 git config user.name "$USER"
 git config user.email "--auto--"
 
 git submodule add https://github.com/owpk/$TERM_UTILS
 git submodule init
-
-stow --adopt -vt $LOCAL_BIN scripts 
 
 # Создаем и переключаемся на новую ветку в основном проекте
 git checkout -b "$USER"
